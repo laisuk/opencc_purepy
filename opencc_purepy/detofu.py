6bb854106466da3b7281584c0f1cb3ce30b68d52
laisuk <laisuk@yahoo.com>
2026-08-22 03:18:26 +0800

    Prevent the use of resolve() which my raise OSError / WinError

--- opencc_purepy/detofu.py
+++ opencc_purepy/detofu.py
@@ -161,7 +161,7 @@ def parse_entries(text: str) -> tuple[DeTofuEntry, ...]:
 
 
 def _builtin_tofu_path() -> Path:
-    return Path(__file__).resolve().parent / "dicts" / "CharactersTofu.txt"
+    return Path(__file__).parent / "dicts" / "CharactersTofu.txt"
 
 
 def get_builtin_entries() -> tuple[DeTofuEntry, ...]:
